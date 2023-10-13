7f7de067a8be052e6bfb35fc2624e1a7df1f4c48
Jone J Jabari <jonejabari123@gmail.com>
2023-10-13 14:32:28 +0200

    Some Changes on awareness

--- app/src/main/java/com/example/gbv/Resources.java
+++ app/src/main/java/com/example/gbv/Resources.java
@@ -1,8 +1,29 @@
 package com.example.gbv;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class Resources extends AppCompatActivity {
 
@@ -10,5 +31,53 @@ public class Resources extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+
+        FirebaseApp.initializeApp(this);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        FirebaseStorage.getInstance().getReference().child("videos").listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onSuccess(ListResult listResult) {
+                ArrayList<Video> arrayList = new ArrayList<>();
+                VideoAdapter adapter = new VideoAdapter(Resources.this, arrayList);
+                adapter.setOnItemClickListener(new VideoAdapter.OnItemClickListener() {
+                    @Override
+                    public void onClick(Video video) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(video.getUrl()));
+                        intent.setDataAndType(Uri.parse(video.getUrl()), "video/*");
+                        startActivity(intent);
+
+                    }
+
+                });
+                recyclerView.setAdapter(adapter);
+                listResult.getItems().forEach(new Consumer<StorageReference>() {
+                    @Override
+                    public void accept(StorageReference storageReference) {
+                        Video video = new Video();
+                        video.setTitle(storageReference.getName());
+                        storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                String url = "https://" + task.getResult().getEncodedAuthority() + task.getResult().getEncodedPath() + "?alt=media&token=" +
+                                        task.getResult().getQueryParameters("token").get(0);
+                                video.setUrl(url);
+                                arrayList.add(video);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Resources.this, "Failed to retrieve videos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/gbv/Video.java
@@ -0,0 +1,20 @@
+package com.example.gbv;
+
+public class Video {
+
+    String title, url;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setUrl(String url){
+        this.url = url;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getUrl(){
+        return url;
+    }
+
+}
